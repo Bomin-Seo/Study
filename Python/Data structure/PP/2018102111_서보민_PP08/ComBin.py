73add27d03d126522636227fbf57226b53a86ae3
Bomin-Seo <94039896+Bomin-Seo@users.noreply.github.com>
2022-08-05 15:28:13 +0900

    Delete Python directory

--- Python/Data structure/PP/2018102111_서보민_PP08/ComBin.py
+++
@@ -1,8 +0,0 @@
-def combinations(group, members):
-    if members == 1:
-        return group
-    elif members == group:
-        return 1
-    else:
-        return (combinations(group - 1, members - 1) + combinations(group - 1, members))
-
--- Python/Study/File_IO/reading&writing/WriteFile.py
+++
@@ -1,7 +0,0 @@
-outfile = open('topics.txt', 'w') # 복수 실행하면 파일 덮어쓰기
-outfile.write('Web-Python')
-outfile.close()
-
-outfile = open('topics.txt', 'a') # data 추가
-outfile.write('software')
-outfile.close()
